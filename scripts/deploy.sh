2002a53411b398341e822db889e8c38996cc1b73
EGOR <egorbas@gmail.com>
2021-01-27 13:11:41 +0300

    Revert "deploy without flyway"
    This reverts commit d91151a9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
-echo 'Maven clean and package...'
+#mvn clean package
 
-mvn clean package
-
-echo 'Copy Application file...'
+echo 'Copy files...'
 
 scp -i ~/.ssh/amazon_ec2_key.pem \
 target/asuod-0.0.1-SNAPSHOT.jar \
-ec2-user@ec2-18-217-69-92.us-east-2.compute.amazonaws.com:/home/ec2-user/asuod/
-
-echo 'Copy DumpDB file...'
-
-scp -i ~/.ssh/amazon_ec2_key.pem \
-dumpDB/asuod_init_dump.sql \
-ec2-user@ec2-18-217-69-92.us-east-2.compute.amazonaws.com:/home/ec2-user/asuod/dumpDB/
+ec2-user@ec2-18-222-193-73.us-east-2.compute.amazonaws.com:/home/ec2-user/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/amazon_ec2_key.pem ec2-user@ec2-18-217-69-92.us-east-2.compute.amazonaws.com <<EOF
+ssh -i ~/.ssh/amazon_ec2_key.pem ec2-user@ec2-18-222-193-73.us-east-2.compute.amazonaws.com <<EOF
 pgrep java | xargs kill -9
-nohup java -jar asuod/asuod-0.0.1-SNAPSHOT.jar > log.txt &
+nohup java -jar asuod-0.0.1-SNAPSHOT.jar > log.txt &
 EOF
 
 echo 'Bye'
- #chmod +x scripts/deploy.sh
- #./scripts/deploy.sh
